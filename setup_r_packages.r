4298ce11cd9dc50676cbd196fb953e4e69d5a25b
Nick Eriksson <nick.eriksson@gmail.com>
2016-11-04 09:40:57 -0700

    more r packages + bioconductor support

--- setup_r_packages.r
+++ setup_r_packages.r
@@ -1,12 +1,13 @@
 #!/usr/bin/env Rscript
 
-required <- c('devtools', 'stringr')
+required <- c('devtools', 'stringr', 'dplyr')
 for (pk in required) {
   if (!requireNamespace(pk, quietly=TRUE)) {
     install.packages(pk)
   }
 }
 
+`%>%` <- dplyr::`%>%`
 
 is_installed <- function(pkg_names) {
   pkg_names %in% installed.packages()[,1]
@@ -14,29 +15,34 @@ is_installed <- function(pkg_names) {
 
 pkgs <- read.table("R_packages")
 names(pkgs) <- 'package_string'
-pkgs$package_name <- unlist(lapply(stringr::str_split(pkgs$package_string, '/'), tail, 1))
 
-pkgs$need_install <- !is_installed(pkgs$package_name)
+pkgs <- pkgs %>%
+    dplyr::mutate(package_name=unlist(lapply(stringr::str_split(package_string, '/|:'), tail, 1)),
+           need_install=!is_installed(package_name))
 
-pkgs$from_github <- grepl('/', pkgs$package_string)
-pkgs$need_install_cran <- pkgs$need_install & !pkgs$from_github
-pkgs$need_install_github <- pkgs$need_install & pkgs$from_github
-print(pkgs)
 
 message(sprintf("Installing %d new packages: %s",
                 sum(pkgs$need_install),
                 paste(pkgs$package_name[pkgs$need_install],
-                      collapse=' ')))
+                      collapse=', ')))
 
-if (any(pkgs$need_install_cran)) {
-    install.packages(pkgs$package_name[pkgs$need_install_cran])
+installer <- function(pkg_string) {
+    if (stringr::str_detect(pkg_string, ':')) {
+        if (!exists("biocLite")) {
+            source("https://bioconductor.org/biocLite.R")
+        }
+        biocLite(stringr::str_split_fixed(pkg_string, ':', 2)[1,2])
+    } else if (stringr::str_detect(pkg_string, '/')) {
+        devtools::install_github(package_string)
+    } else {
+        install.packages(pkg_string)
+    }
 }
 
-if (any(pkgs$need_install_github)) {
-    devtools::install_github(pkgs$package_string[pkgs$need_install_github])
-}
+tmp <- sapply(dplyr::filter(pkgs, need_install)$package_string, installer)
 
 pkgs$final_installed <- is_installed(pkgs$package_name)
+
 if (!all(pkgs$final_installed)) {
     message("Install failed for: ", paste(pkgs$package_name[!pkgs$final_installed], collapse=' '))
 }
