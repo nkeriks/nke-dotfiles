93fbdbae7612eebbf57a4af496135d65a1b9f13a
Nick Eriksson <nick@calicolabs.com>
2019-02-17 16:44:01 -0500

    updated R package list + apt list of headers

---
+++ install_debian_pkgs_for_r.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# headers needed to compile R packages
+sudo apt install \
+    libpoppler-cpp-dev \
+    libudunits2-dev \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    libwebp-dev \
+    libprotobuf-dev \
+    libprotoc-dev \
+    libmariadbclient-dev \
+    libsodium-dev \
+    libpq-dev \
+    cargo \
+    libavfilter-dev \
+    libmagick++-dev \
+    libcairo2-dev \
+    libglpk-dev
+
